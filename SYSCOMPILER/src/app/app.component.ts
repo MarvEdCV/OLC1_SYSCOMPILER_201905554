5a42f3f14bb7c14b2e0c61d49ef853d1317ee58d
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-10-05 12:33:16 -0600

    UPDATE Interfaz
    Se terminó toda la interfaz visual sin funcionalidades aún.

--- SYSCOMPILER/src/app/app.component.ts
+++ SYSCOMPILER/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component,ElementRef, OnInit, ViewChild } from '@angular/core';
-import parser from './interpreter/grammar/grammar.js';
+//import parser from './interpreter/grammar/grammar.js';
 import { Expresion } from './interpreter/Expresion/Expresion.js'; 
 
 import * as ace from 'ace-builds'; // ace module ..
 import 'ace-builds/src-noconflict/ext-beautify';
-import 'ace-builds/src-noconflict/mode-typescript'
-import 'ace-builds/src-noconflict/theme-tomorrow_night_eighties';;
+import 'ace-builds/src-noconflict/mode-typescript';
+import 'ace-builds/src-noconflict/theme-tomorrow_night_eighties';
 const THEME = 'ace/theme/tomorrow_night_eighties';
 const LANG = 'ace/mode/typescript';
 
@@ -26,7 +26,10 @@ export class AppComponent implements OnInit {
     this.editorBeautify = ace.require('ace/ext/beautify');
     const element = this.codeEditorElmRef.nativeElement;
     const editorOptions: Partial<ace.Ace.EditorOptions> = {
-      highlightActiveLine: false,
+      cursorStyle: "ace",
+      showLineNumbers:true,
+      printMarginColumn: 100,
+      highlightActiveLine: true,
       minLines: 45,
       maxLines: Infinity,
       maxPixelHeight: 800,
@@ -45,10 +48,10 @@ export class AppComponent implements OnInit {
       alert("Entrada vacia")
       return
     }
-  const ast = parser.parse(entrada)
-   console.log(ast)
+  //const ast = parser.parse(entrada)
+   //console.log(ast)
     try {
-    console.log(ast.execute());
+    //console.log(ast.execute());
     } catch (error) {
       console.log(error)
     }

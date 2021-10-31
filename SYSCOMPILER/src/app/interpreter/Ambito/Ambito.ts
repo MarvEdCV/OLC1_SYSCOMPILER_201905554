156d9d3adececfaba3c7de5cc1e94d199402fe00
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-10-31 00:23:46 -0600

    UPDATE changeVal
    Se creo un método en el ambito el cual elimina un simbolo creado anteriormente por medio de una declaración sin asignación, y lo vuelve a sobreescribir en el mismo ambito pero con su valor ya asignado.

--- SYSCOMPILER/src/app/interpreter/Ambito/Ambito.ts
+++ SYSCOMPILER/src/app/interpreter/Ambito/Ambito.ts
@@ -1,5 +1,7 @@
+
 import { Error } from "../Error/Error";
 import { Type } from "../Expresion/Retorno";
+import { Declaracion } from "../Instruccion/Declaracion";
 import { Simbolo } from "./Simbolo"
 
 export class Ambito {
@@ -33,6 +35,19 @@ export class Ambito {
             env = env.anterior
         }
         return null;
+    }
+    
+    public changeVal(id:string,value:any,type:Type,line,column){
+        let env:Ambito|null=this;
+        while (env!=null){
+            if(env.variables.has(id)){
+                //Si encuentra el id en el ambito borramos el temporal y lo sobreinscribimos
+                env.variables.delete(id);
+                //Sobreincribir
+                env.setVal(id,value,type,line,column)
+            }
+            env = env.anterior
+        }
     }
 
 }

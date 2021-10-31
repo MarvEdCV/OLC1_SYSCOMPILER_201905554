8d3ac270c9162bb5e51838d7db008aed430a8c3a
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-10-31 00:39:30 -0600

    UPDATE Declaracion.ts
    Se le agregaron validacione para que el tipo de dato que se le asigna a la variable sea el mismo tipo de dato que el que se esta declarando. Para evitar errores de este tipo:
    Int numero="Hola";

--- SYSCOMPILER/src/app/interpreter/Instruccion/Declaracion.ts
+++ SYSCOMPILER/src/app/interpreter/Instruccion/Declaracion.ts
@@ -1,21 +1,46 @@
 import { Expresion } from "../Expresion/Expresion";
 import { Instruccion } from "./Instruccion"
 import { Ambito } from '../Ambito/Ambito';
+import { Type } from "../Expresion/Retorno";
+import { Error } from "../Error/Error";
 
 export class Declaracion extends Instruccion {
 
     private id: string;
     private value: Expresion;
-
-    constructor(id: string, value: Expresion, line: number, column: number) {
+    private type:string;
+    constructor(type:string,id: string, value: Expresion, line: number, column: number) {
         super(line, column)
         this.id = id
         this.value = value
+        this.type = type
     }
 
     public execute(ambito: Ambito) {
         let val = this.value.execute(ambito)
-        ambito.setVal(this.id, val.value, val.type, this.line, this.column)
+        let typeenum;
+        if(this.type.toUpperCase()=="INT"){
+            typeenum = 0;
+            
+        }else if(this.type.toUpperCase()=="DOUBLE"){
+            typeenum = 1;
+            
+        }else if(this.type.toUpperCase()=="BOOLEAN"){
+            typeenum = 2;
+            
+        }else if(this.type.toUpperCase()=="CHAR"){
+            typeenum = 3;
+            
+        }else if(this.type.toUpperCase()=="STRING"){
+            typeenum=4;
+            
+        }
+        if(typeenum==val.type){
+            ambito.setVal(this.id, val.value, val.type, this.line, this.column)
+        }else{
+            throw new Error(this.line, this.column, 'Semantico', 'No se puede asignar este valor a este tipo de datox')
+        }
         console.log(ambito.variables)
+        
     }
 }

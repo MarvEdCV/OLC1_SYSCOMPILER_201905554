e373517ca954958bc2c80935cbce14c090c9c8da
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-03 17:47:04 -0600

    Create If.ts
    Se crea esta clase para generarle funcionalidad a IF, IF ELSE y ELSE.

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/If.ts
@@ -0,0 +1,26 @@
+import { Instruccion } from "./Instruccion";
+import { Error } from "../Error/Error";
+import { Type } from "../Expresion/Retorno";
+import { Ambito } from "../Ambito/Ambito";
+
+
+export class If extends Instruccion {
+    constructor(private condicion, private cuerpo: Instruccion, private Else: Instruccion, line: number, column: number) {
+        super(line, column)
+    }
+    public execute(ambito: Ambito) {
+        const value = this.condicion.execute(ambito)
+
+        if (value.type != Type.BOOLEAN) throw new Error(this.line, this.column, 'Semantico', 'La condicion evaluada no es de tipo Boleana')
+        // true - false
+
+        if (value.value) {
+            return this.cuerpo.execute(ambito)
+
+        } else if (this.Else != null) {
+            return this.Else.execute(ambito)
+        }
+
+    }
+
+}

4ca3bf5869574f090a0968069638a3c7a714148a
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-03 17:45:41 -0600

    Create Statement.ts
    Se crea para poder crear nuevos ambitos y ejecutar todas las instrucciones que esten dentro de él. Que esten encerrados entre dos llaves.

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/Statement.ts
@@ -0,0 +1,23 @@
+ 
+import { Ambito } from "../Ambito/Ambito";
+import { Instruccion } from "./Instruccion";
+
+export class Statement extends Instruccion {
+    constructor(private code: Instruccion[], line: number, column: number) {
+        super(line, column)
+    }
+
+    public execute(ambito: Ambito) {
+        const NuevoAmbito = new Ambito(ambito)
+        for (const instruccion of this.code) {
+            try {
+                const element = instruccion.execute(NuevoAmbito)
+
+                if (element != null && element != undefined) return element
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}

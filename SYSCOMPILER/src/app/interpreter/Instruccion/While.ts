fdd5ab125ad4901642e59ee3c43890ed394d3fb5
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-03 17:49:09 -0600

    Create While.ts
    Se crea esta clase para poder ejecutar el ciclo While junto con su continue y si Break

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/While.ts
@@ -0,0 +1,30 @@
+import { Instruccion } from "./Instruccion"
+import { Expresion } from "../Expresion/Expresion"
+import { Type } from "../Expresion/Retorno"
+import { Ambito } from "../Ambito/Ambito"
+import { Error } from "../Error/Error"
+export class While extends Instruccion {
+    constructor(private condicion: Expresion, private cuerpo: Instruccion, line: number, column: number) {
+        super(line, column)
+    }
+
+    public execute(ambito: Ambito) {
+        let value = this.condicion.execute(ambito)
+
+        if (value.type != Type.BOOLEAN) throw new Error(this.line, this.column, 'Semantico', "La condicion a evaluar no es de tipo boolean")
+
+        while (value.value) {
+            const retorno = this.cuerpo.execute(ambito)
+            if (retorno != null && retorno != undefined) {
+                if (retorno.type == 'Break') {
+                    break
+                } else if (retorno.type == 'Continue') {
+                    continue
+                }
+            }
+
+
+            value = this.condicion.execute(ambito)
+        }
+    }
+}

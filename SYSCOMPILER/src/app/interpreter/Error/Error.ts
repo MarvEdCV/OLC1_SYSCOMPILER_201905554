cd01ce7d249753771e7c90ef53b177660b728edf
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-03 17:42:00 -0600

    Update Error.ts
    Se solucionan errores de impresiones con mala estetica.

--- SYSCOMPILER/src/app/interpreter/Error/Error.ts
+++ SYSCOMPILER/src/app/interpreter/Error/Error.ts
@@ -1,9 +1,9 @@
-export let MensajeError;
+export let MensajeError="";
 
 export class Error {
     constructor(public linea: number, public columna: number, public tipo: string, public mensaje: string) {
         //console.log('Error:',this.tipo,'\nMensaje:',this.mensaje,'\nLinea:',this.linea,'\nColumna:',this.columna)
-        MensajeError = "Error:"+this.tipo.toString()+"\nMensaje:"+this.mensaje.toString()+"\nLinea:"+this.linea.toString()+"\nColumna:"+this.columna.toString();
+        MensajeError = "\nError:"+this.tipo.toString()+"\nMensaje:"+this.mensaje.toString()+"\nLinea:"+this.linea.toString()+"\nColumna:"+this.columna.toString();
         console.log(MensajeError);
     }
 }

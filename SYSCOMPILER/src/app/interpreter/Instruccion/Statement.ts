8d721dffc3c3219e198aa4e6f88812ac01c93673
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-04 16:13:21 -0600

    Update Statement.ts
    Se agrega una condición para que no pueda crear un método dentro de otro método.

--- SYSCOMPILER/src/app/interpreter/Instruccion/Statement.ts
+++ SYSCOMPILER/src/app/interpreter/Instruccion/Statement.ts
@@ -1,6 +1,8 @@
  
 import { Ambito } from "../Ambito/Ambito";
 import { Instruccion } from "./Instruccion";
+import { Metodo } from "./Metodo";
+import { Error } from "../Error/Error";
 
 export class Statement extends Instruccion {
     constructor(private code: Instruccion[], line: number, column: number) {
@@ -10,6 +12,9 @@ export class Statement extends Instruccion {
     public execute(ambito: Ambito) {
         const NuevoAmbito = new Ambito(ambito)
         for (const instruccion of this.code) {
+            if(instruccion instanceof Metodo){
+                throw  new Error(this.line, this.column, 'Semantico','No se permiten metodos dentro de otros metodos');
+            }
             try {
                 const element = instruccion.execute(NuevoAmbito)
 

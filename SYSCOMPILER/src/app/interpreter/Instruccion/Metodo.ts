bd120c635218833ad469c453c947189522be56e8
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-04 16:09:30 -0600

    Create Metodo.ts
    Se crea esta clase para guardar metodos declarados(de momento solamente métodos de tipo void).

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/Metodo.ts
@@ -0,0 +1,12 @@
+import { Instruccion } from "./Instruccion";
+import { Ambito } from "../Ambito/Ambito";
+import { Declaracion } from "./Declaracion";
+export class Metodo extends Instruccion {
+    constructor(public id: string, public statment: Instruccion, public parametros: Array<string>, line: number, column: number) {
+        super(line, column);
+    }
+
+    public execute(ambito: Ambito): any {
+        ambito.SaveMethod(this.id,this)
+    }
+}

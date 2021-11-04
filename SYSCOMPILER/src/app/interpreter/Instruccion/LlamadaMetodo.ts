3e6b19a4073dd485dc75046332c1acb675fc188f
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-04 16:12:56 -0600

    Create LlamadaMetodo.ts
    Se crea esta clase para poder ejecutar una función al ser llamada, se hacen varias validaciones tales como, que el metodo no se encuentre, o si el número de parametros es diferente en la declaración que en la llamada, y también que los tipos de datos sean iguales tanto en la declaración como en la llamada.

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/LlamadaMetodo.ts
@@ -0,0 +1,31 @@
+import { Instruccion } from "./Instruccion";
+import { Ambito } from "../Ambito/Ambito";
+import { Error } from "../Error/Error";
+import { Expresion } from "../Expresion/Expresion";
+import { Metodo } from "./Metodo";
+import { Type } from "../Expresion/Retorno";
+export class LlamadaMetodo extends Instruccion {
+    constructor(private id: string, private expresiones: Array<Expresion>, line: number, column: number) {
+        super(line, column);
+    }
+    public execute(ambito: Ambito) {
+        const method = ambito.GetMethod(this.id);
+        if (method == undefined) throw new Error(this.line, this.column, 'Semantico', `Funcion ${this.id} no encontrada`)
+        if (this.expresiones.length != method.parametros.length) throw new Error(this.line, this.column, 'Semantico', "Cantidad de parametros incorrecta\nlos ingresados en la llamada son: "+this.expresiones.length+" y tendrian que ser: "+method.parametros.length)
+               
+        //VAlidar tambien que los parametros de los dos sean del mismo tipo.
+        const newEnv = new Ambito(ambito/*.getGlobal()*/);
+        for (let i = 0; i < this.expresiones.length; i++) {
+            const value = this.expresiones[i].execute(ambito);
+            let x=Number(method.parametros[i][0]);
+            let y=Number(value.type)
+            console.log(x==y); 
+            if(x==y){
+                newEnv.setVal(method.parametros[i][1], value.value, value.type, this.line, this.column);
+            }else{
+                throw new Error(this.line, this.column, 'Semantico', 'El tipo del parámetro numero--> '+(i+1)+'\ndeclarado en el método no es el mismo tipo que el de la llamada.')
+            }            
+        }
+        method.statment.execute(newEnv);
+    }
+}

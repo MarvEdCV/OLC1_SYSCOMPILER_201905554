b575409a80be8e23b0475ea75287afa9ab69ece8
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-05 19:24:17 -0600

    Update Error.ts
    Se soluciona la impresion de errores para que no imprima solo el útlimo.

--- SYSCOMPILER/src/app/interpreter/Error/Error.ts
+++ SYSCOMPILER/src/app/interpreter/Error/Error.ts
@@ -3,7 +3,7 @@ export let MensajeError="";
 export class Error {
     constructor(public linea: number, public columna: number, public tipo: string, public mensaje: string) {
         //console.log('Error:',this.tipo,'\nMensaje:',this.mensaje,'\nLinea:',this.linea,'\nColumna:',this.columna)
-        MensajeError = "\nError:"+this.tipo.toString()+"\nMensaje:"+this.mensaje.toString()+"\nLinea:"+this.linea.toString()+"\nColumna:"+this.columna.toString();
+        MensajeError =MensajeError+ "\n-->Error:"+this.tipo.toString()+"\n-->Mensaje:"+this.mensaje.toString()+"\n-->Linea:"+this.linea.toString()+"\n-->Columna:"+this.columna.toString()+"\n";
         console.log(MensajeError);
     }
 }

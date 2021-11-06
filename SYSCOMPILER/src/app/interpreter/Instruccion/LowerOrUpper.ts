c1f61c7af8dd105d243cd4570c275a0f1a2d1142
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-06 10:20:35 -0600

    Update LowerOrUpper.ts
    En esta clase se agregan todas las validaciones y para todas las funciones nativas que no lleven arreglos o listas.

--- SYSCOMPILER/src/app/interpreter/Instruccion/LowerOrUpper.ts
+++ SYSCOMPILER/src/app/interpreter/Instruccion/LowerOrUpper.ts
@@ -13,6 +13,25 @@ export class LowerOrUpper extends Instruccion {
     }
     public execute(ambito:Ambito):Retorno{
         const executeexpresion = this.value.execute(ambito);
+        let tipo;
+        if(this.type=="typeof"){
+            if(executeexpresion.type==0){
+                tipo="Int";
+            }if(executeexpresion.type==1){
+                tipo="Double";
+            }if(executeexpresion.type==2){
+                tipo="Boolean";
+            }if(executeexpresion.type==3){
+                tipo="Char";
+            }if(executeexpresion.type==4){
+                tipo="String";
+            }
+            return {value:tipo,type:Type.STRING}
+        }if(this.type=="tostring"){
+            if(executeexpresion.type != Type.CHAR){
+                return {value:"\""+executeexpresion.value.toString()+"\"",type:Type.STRING}
+            }else{throw new Error(this.line, this.column, 'Semantico', 'No se puede usar este método para este tipo de char.')}            
+        }
         if(executeexpresion.type == Type.STRING){
             if(this.type == "tolower"){
                 executeexpresion.value = executeexpresion.value.toLowerCase();
@@ -21,12 +40,19 @@ export class LowerOrUpper extends Instruccion {
                 executeexpresion.value = executeexpresion.value.toUpperCase();
                 return {value:executeexpresion.value,type:Type.STRING}
             }            
-        }
-            throw new Error(this.line, this.column, 'Semantico', 'No se puede usar este método para este tipo de dato.')
-        
+        }else if(executeexpresion.type == Type.DOUBLE || executeexpresion.type == Type.INT ){
+            if(this.type == "truncate"){
+                executeexpresion.value = Math.trunc(executeexpresion.value);
+                return {value:executeexpresion.value,type:Type.INT}
+            }else if(this.type == "round"){
+                executeexpresion.value = Math.round(executeexpresion.value);
+                return {value:executeexpresion.value,type:Type.INT}
+            }  
     }
+    throw new Error(this.line, this.column, 'Semantico', 'No se puede usar este método para este tipo de dato.')
+}
     public getCodigoAST():{codigo:string,nombreNodo:string}{
 
-        return {codigo:"codigo" , nombreNodo: "nombreNodoPrincipal.toString()"  };
+        return {codigo:"LOWERORUPPER" , nombreNodo: "LOWERORUPPER"  };
     }
 }

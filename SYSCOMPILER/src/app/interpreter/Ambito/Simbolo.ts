0bf1a32c2821c3ecb5eb218589eecee2543fc239
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-05 19:24:38 -0600

    Update Simbolo.ts
    Se le agrega linea y columna a cada simbolo, para poder imprimirlo en la tabla de simbolos.

--- SYSCOMPILER/src/app/interpreter/Ambito/Simbolo.ts
+++ SYSCOMPILER/src/app/interpreter/Ambito/Simbolo.ts
@@ -5,10 +5,14 @@ export class Simbolo {
     public id: string;
     public type: Type;
     public valor: any;
-    constructor(id: string, type: any, valor: any) {
+    public line: number;
+    public column: number;
+    constructor(id: string, type: any, valor: any,line:number,column:number) {
         this.id = id;
         this.type = type;
         this.valor = valor;
+        this.line = line;
+        this.column = column;
         
     }
 }

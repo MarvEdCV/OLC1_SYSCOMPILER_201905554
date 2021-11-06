9c54693a21b10f6f4bc3f30452c91378d165f0d6
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-05 19:25:59 -0600

    Update Ambito.ts
    Se actualiza agregando en un arreglo todos los simbolos, cada vez que se usa el método SetVal para agregar variables a la tabla de simbolos. Validando de que no se dupliquen aunque se declaren o asignen denuevo.

--- SYSCOMPILER/src/app/interpreter/Ambito/Ambito.ts
+++ SYSCOMPILER/src/app/interpreter/Ambito/Ambito.ts
@@ -4,11 +4,13 @@ import { Type } from "../Expresion/Retorno";
 import { Declaracion } from "../Instruccion/Declaracion";
 import { Metodo } from "../Instruccion/Metodo";
 import { Simbolo } from "./Simbolo"
-
+export let listavar:Array<any>=[];
 export class Ambito {
     public variables: Map<string, Simbolo>
     public metodos: Map<string,Metodo>
+    
 
+    
     constructor(public anterior: Ambito | null) {
         this.variables = new Map();
         this.metodos  = new Map();
@@ -20,12 +22,28 @@ export class Ambito {
         while (env != null) {
             if (env.variables.has(id)) {
                 const val = env.variables.get(id);
-                if (val?.type == type) env.variables.set(id, new Simbolo(value, id, type))
-                throw new Error(line, column, 'Semantico', 'No se puede asignar: ' + value + ' a ' + id)
+                if (val?.type == type) {
+                    env.variables.set(id, new Simbolo(value, id, type,line,column))
+                }
+                
+                else{throw new Error(line, column, 'Semantico', 'No se puede asignar: ' + value + ' a ' + id)}
+                
             }
             env = env.anterior
         }
-        this.variables.set(id, new Simbolo(id,type,value))
+        this.variables.set(id, new Simbolo(id,type,value,line,column))
+        for(const simboloactual of this.variables){
+            if(!(this.existeya(listavar,simboloactual[1].id))){
+                listavar.push(simboloactual);
+            }    
+     }
+}
+    public existeya(lista:Array<any>,id:any):boolean{        
+        for (let index = 0; index < lista.length; index++) {
+            if(id==lista[index][1].id){
+                return true;
+            }           
+        }return false;
     }
 
     public getVal(id: string): Simbolo|null|undefined {
@@ -54,5 +72,12 @@ export class Ambito {
        
         return null;
 
+    }
+    public getGlobal(): Ambito {
+        let env: Ambito | null = this;
+        while (env?.anterior != null) {
+            env = env.anterior;
+        }
+        return env;
     }
 }

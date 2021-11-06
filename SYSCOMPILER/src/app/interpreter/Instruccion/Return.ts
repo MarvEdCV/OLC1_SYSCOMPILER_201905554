3188a5b4bced7602a34fa7a572d34aea665f7d17
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2021-11-05 19:18:11 -0600

    Create Return.ts
    Se crea esta clase para usarla posteriormente en las funciones que tienen que retornar algun dato de cualquier tipo aceptado.

---
+++ SYSCOMPILER/src/app/interpreter/Instruccion/Return.ts
@@ -0,0 +1,10 @@
+import { Ambito } from "../Ambito/Ambito";
+import { Instruccion } from "./Instruccion";
+export class Return extends Instruccion {
+    constructor(line,column) {
+        super(line,column)
+    }
+    public execute(ambito:Ambito){
+        return {type:'Return',line:this.line,column:this.column}
+    }
+}
